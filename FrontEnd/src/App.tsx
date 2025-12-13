40485c8ba8d27c3e0851f0a39396f9b279873260
ErangaDissanayaka <erangamaheshdissanayake91@gmail.com>
2025-12-13 11:29:20 +0530

    Adds main navigation, user pages, and responsive NavBar
    Introduces full routing for user-facing pages and enhances navigation with a responsive NavBar and footer, excluding admin routes. Improves user experience with a mobile-friendly menu and toast notifications. Removes unnecessary React StrictMode import to streamline setup.

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import NavBar from './Components/NavBar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home'
+import Movies from './Pages/Movies'
+import MovieDetails from './Pages/MovieDetails'
+import SeatLayout from './Pages/SeatLayout'
+import MyBookings from './Pages/MyBookings'
+import Favourite from './Pages/Favourite'
+import { Toaster } from 'react-hot-toast'
+import Footer from './Components/Footer'
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
   return (
-    <div>
-      
-    </div>
+    <>
+      <Toaster/>
+      {!isAdminRoute && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:dates' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favourite' element={<Favourite />} />
+      </Routes>
+        {!isAdminRoute && <Footer />}
+    </>
   )
 }
 
