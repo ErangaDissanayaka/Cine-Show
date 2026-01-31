31f289bb1b63b3f6ea4534763ddcd1b3364704b4
ErangaDissanayaka <erangamaheshdissanayake91@gmail.com>
2026-01-31 22:06:45 +0530

    Enhances featured section with movie cards and fixes navigation
    Displays a selection of movie cards in the featured section for improved user engagement and visual appeal. Updates navigation button handlers to use correct JavaScript statement separators, preventing potential logic issues.

--- FrontEnd/src/Components/NavBar.tsx
+++ FrontEnd/src/Components/NavBar.tsx
@@ -32,11 +32,11 @@ const NavBar = () => {
           onClick={() => setIsOpen(!isOpen)}
         />
 
-        <Link onClick={() => { scrollTo(0, 0), setIsOpen(false) }} to='/home' >Home</Link>
-        <Link onClick={() => { scrollTo(0, 0), setIsOpen(false) }} to='/movies' >Movies</Link>
-        <Link onClick={() => { scrollTo(0, 0), setIsOpen(false) }} to='/' >Theaters</Link>
-        <Link onClick={() => { scrollTo(0, 0), setIsOpen(false) }} to='/' >Releases</Link>
-        <Link onClick={() => { scrollTo(0, 0), setIsOpen(false) }} to='/favourite' >Favourites</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false) }} to='/home' >Home</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false) }} to='/movies' >Movies</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false) }} to='/' >Theaters</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false) }} to='/' >Releases</Link>
+        <Link onClick={() => { scrollTo(0, 0); setIsOpen(false) }} to='/favourite' >Favourites</Link>
       </div>
 
       <div className="flex items-center gap-8">
