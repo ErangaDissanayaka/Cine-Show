28df857abc91a7e3219f83274c26e343a994690b
ErangaDissanayaka <erangamaheshdissanayake91@gmail.com>
2026-03-01 11:49:55 +0530

    Refactor MovieCard component layout and integrate timeFormat utility for runtime display

--- FrontEnd/src/Components/MovieCard.tsx
+++ FrontEnd/src/Components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { StarIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import timeFormat from "../lib/timeFormat";
 
 interface MovieCardProps {
   movie: {
@@ -13,32 +14,51 @@ interface MovieCardProps {
 }
 
 const MovieCard = ({ movie }: MovieCardProps) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl
-    hover:-translate-y-1 transition duration-300 w-70'>
+    <div
+      className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl
+    hover:-translate-y-1 transition duration-300 w-70"
+    >
+      <img
+        onClick={() => {
+          (navigate(`/movies/${movie.id}`), scrollTo(0, 0));
+        }}
+        src={movie.backdrop_path}
+        alt=""
+        className="rounded-lg h-52 w-full
+        object-cover object-bottom-right cursor-pointer"
+      />
 
-        <img onClick={()=>{navigate(`/movies/${movie.id}`), scrollTo(0,0)}}
-        src={movie.backdrop_path} alt='' className='rounded-lg h-52 w-full
-        object-cover object-bottom-right cursor-pointer'/>
+      <p className="text-sm text-gray-400 mt-2">
+        {new Date(movie.release_date).getFullYear()}.{" "}
+        {movie.genres
+          .slice(0, 2)
+          .map((genre) => genre.name)
+          .join(" | ")}{" "}
+        . {timeFormat(movie.runtime)}
+      </p>
 
-        <p className="text-sm text-gray-400 mt-2">
-         {new Date(movie.release_date).getFullYear()}. {movie.genres.slice(0,2).map
-         (genre => genre.name).join(" | ")} . {movie.runtime}
-         </p> 
+      <div className="flex items-center justify-between mt-4 pb-3">
+        <button
+          onClick={() => {
+            navigate(`/movies/${movie.id}`);
+            scrollTo(0, 0);
+          }}
+          className="px-4 py-2 text-xs bg-primary hover:bg-primary-dull
+            transition rounded-full font-medium cursor-pointer"
+        >
+          Buy Tickets
+        </button>
 
-        <div className="flex items-center justify-between mt-4 pb-3">
-            <button onClick={()=>{navigate(`/movies/${movie.id}`); scrollTo(0,0);}} className="px-4 py-2 text-xs bg-primary hover:bg-primary-dull
-            transition rounded-full font-medium cursor-pointer">Buy Tickets</button>
-            
-            <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
-                <StarIcon className="w-4 h-4 text-primary fill-primary" />
-                {movie.vote_average.toFixed(1)}
-            </p>
-        </div>
+        <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+          <StarIcon className="w-4 h-4 text-primary fill-primary" />
+          {movie.vote_average.toFixed(1)}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
