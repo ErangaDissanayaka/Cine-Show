1a944f3c6f7919fa9b1885f1735bedacd4c0f3aa
ErangaDissanayaka <erangamaheshdissanayake91@gmail.com>
2026-03-02 10:59:46 +0530

    Enhance Footer component with company info and contact details; add TrailerSection to Home page

--- FrontEnd/src/Pages/Home.tsx
+++ FrontEnd/src/Pages/Home.tsx
@@ -1,14 +1,15 @@
-import FeaturedSection from '../Components/FeaturedSection'
-import HeroSection from '../Components/HeroSection'
+import FeaturedSection from "../Components/FeaturedSection";
+import HeroSection from "../Components/HeroSection";
+import TrailerSection from "../Components/TrailerSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <FeaturedSection />
+      <TrailerSection />
     </>
-  
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
